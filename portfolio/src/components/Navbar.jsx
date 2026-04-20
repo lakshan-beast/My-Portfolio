86e12d5f0066e7d356fad88b9c3800736ad91b83
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-04-20 18:10:14 +0530

    clean and fix bug

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-function Navbar() {}
+// import { FaCopyright, FaGem } from "react-icons/fa6";
+// import { FiInstagram, FiTwitter, FiFacebook, FiHeart } from "react-icons/fi";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__container">
+        <div className="navbar__logo">
+          <h1>Lakshan</h1>
+        </div>
+        {/* 
+        <div className="navbar__menu">
+            
+        </div> */}
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
