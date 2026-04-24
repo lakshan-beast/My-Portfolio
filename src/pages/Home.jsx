f7d40c46608f4f199a9e8c02055c7212e212361d
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-04-24 15:17:24 +0530

    add  home section and clean it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,26 +2,28 @@ import React from "react";
 
 import { FiGithub, FiX, FiLinkedin } from "react-icons/fi";
 import { FaRegEnvelope } from "react-icons/fa";
-// import { SiFreelancer } from "react-icons/si";
 
 const Home = () => {
   return (
-    <section className="hero">
+    <section className="hero" id="hero">
       <div className="hero-container">
         <h1 className="header" data-aos="fade-up">
           Crafting High-Perfomance
           <span className="highlight"> User Interfaces</span> with React.
         </h1>
+
         <p className="description" data-aos="fade-up">
           Specailizing in Building scalable, accessible, & pixel perfect web
           applications. I bridge the gap between complex backend logic &
           intuitive frontend experience.
         </p>
 
+        {/* hero buttons */}
         <div className="hero-buttons">
           <a href="#projects" className="btn-primary" data-aos="fade-right">
             View Portfolio
           </a>
+
           <a
             href="/my_resume.pdf"
             download="Lakshan_Sandeepa_CV.pdf"
@@ -31,6 +33,7 @@ const Home = () => {
           </a>
         </div>
 
+        {/* socail media links */}
         <div className="hero-socail" data-aos="zoom-in">
           <a href="https://github.com/lakshan-beast" target="_blank">
             <FiGithub />
@@ -40,9 +43,6 @@ const Home = () => {
             target="_blank">
             <FiLinkedin />
           </a>
-          {/* <a href="#">
-            <SiFreelancer />
-          </a> */}
           <a href="mailto:lakshansandeepa0305@gmail.com">
             <FaRegEnvelope />
           </a>
