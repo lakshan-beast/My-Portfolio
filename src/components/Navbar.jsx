5590bf037705ae064c9a77265f44b69d820d6785
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-04-24 08:42:52 +0530

    finifhed navbar content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { TiThMenuOutline } from "react-icons/ti";
+import { IoMdClose } from "react-icons/io";
 
-// import { FaCopyright, FaGem } from "react-icons/fa6";
-// import { FiInstagram, FiTwitter, FiFacebook, FiHeart } from "react-icons/fi";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <div className="navbar__logo">
-          <h1>Lakshan</h1>
+          <h1>
+            Lakshan<span>.</span>
+          </h1>
         </div>
 
-        <div className="navbar__menu">
-          <a href="#">A</a>
-          <a href="#">A</a>
-          <a href="#">A</a>
-          <a href="#">A</a>
-          <a href="#">A</a>
+        {/* Desktop Links */}
+        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+          <li>
+            <a href="#hero" onClick={() => setIsOpen(false)}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" onClick={() => setIsOpen(false)}>
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" onClick={() => setIsOpen(false)}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#project" onClick={() => setIsOpen(false)}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={() => setIsOpen(false)}>
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        {/* Mobile Toggle Button */}
+        <div className="menu-icon" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <IoMdClose /> : <TiThMenuOutline />}
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
