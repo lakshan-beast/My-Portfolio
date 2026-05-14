d6be4b81940debbff23ea1549d2362a6bc8ea571
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-14 07:49:30 +0530

    fix: enhance AOS animations in Skills section for improved visual engagement

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -31,24 +31,29 @@ const Skills = () => {
     { name: "SASS / SCSS", level: "Intermediate", date: "March 10, 2026" },
     { name: "CSS3 / HTML5", level: "Advanced", date: "January 02, 2026" },
     { name: "Git & Github", level: "Basic", date: "February 01, 2026" },
-    { name: "Tailwind CSS", level: "Pending", date: "June 01, 2026" },
     { name: "FireBase", level: "Basic", date: "May 01, 2026" },
+    { name: "EmailJS", level: "Basic", date: "April 20, 2026" },
+    { name: "Tailwind CSS", level: "Pending", date: "June 01, 2026" },
   ];
 
   return (
     <section className="skills" id="skills" data-aos-offset="0">
-      <div className="skills-container">
-        <h2 className="section-title">Technical Toolbox</h2>
+      <div className="skills-container" data-aos="fade-up">
+        <h2 className="section-title" data-aos="fade-right">
+          Technical Toolbox
+        </h2>
 
         <div className="skills-grid">
           {skillList.map((skill, index) => (
             <div className="skill-card" key={index} data-aos="fade-up">
-              <h3>{skill.name}</h3>
+              <h3 data-aos="fade-down">{skill.name}</h3>
               <span className="exp-tag">{calculateExperience(skill.date)}</span>
-              <p>{skill.level} </p>
+              <p data-aos="fade-right">{skill.level} </p>
 
               <div className="progress-bar">
-                <div className={`progress ${skill.level.toLowerCase()}`}></div>
+                <div
+                  data-aos="fade-right"
+                  className={`progress ${skill.level.toLowerCase()}`}></div>
               </div>
             </div>
           ))}
