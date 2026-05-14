f26562973d1af3fbdc7e74b06bee195cd43c93c3
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-05-14 07:48:01 +0530

    fix: enhance AOS animations for improved user experience in About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,14 @@ import HeroImage from "../assets/hero.jpg";
 const About = () => {
   return (
     <section className="about" id="about" data-aos-offset="0">
-      <div className="about-container">
-        <h2 className="section-title">Professional Profile</h2>
+      <div className="about-container" data-aos="fade-up">
+        <h2 className="section-title" data-aos="fade-up">
+          Professional Profile
+        </h2>
 
         <div className="about-wrapper">
           <div className="about-text">
-            <p data-aos="fade-right">
+            <p data-aos="fade-up">
               A dedicated <strong>Frontend Developer</strong> with a strong
               foundation in modern JavaSctipt & React. My approach focuses on
               writing clean, maintainable code & optimizing perfomance for the
@@ -17,19 +19,19 @@ const About = () => {
             </p>
 
             <div className="about-stats" data-aos="fade-up">
-              <div className="stat-item">
+              <div className="stat-item" data-aos="fade-right">
                 <span className="number">Self-Taught</span>
                 <span className="label">Expertise</span>
               </div>
 
-              <div className="stat-item">
+              <div className="stat-item" data-aos="fade-right">
                 <span className="number">React</span>
                 <span className="label">Specailist</span>
               </div>
             </div>
           </div>
 
-          <div className="about-image" data-aos="fade-up">
+          <div className="about-image" data-aos="fade-left">
             <img src={HeroImage} alt="Hero Image" />
           </div>
         </div>
