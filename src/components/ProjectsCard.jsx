5ad86e90e54e2060ade989f84beac3e63d619a8d
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-08-01 09:30:30 +0530

    feat: add Unseen Tapro project to ProjectsCard component

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -6,6 +6,7 @@ import Zestara from "../assets/Zestara.webp";
 import Info_Lanka from "../assets/Info.Lanka.webp";
 import Educa from "../assets/educa.png";
 import Dream_Tracker from "../assets/Dream-Tracker.webp";
+import Unseen_Tapro from "../assets/Unseen-Tapro.webp";
 
 const Projects = () => {
   const projectsList = [
@@ -22,29 +23,30 @@ const Projects = () => {
       linkedin: "",
     },
 
-    // info.lanka project
+    // Unseen Tapro
     {
-      image: Info_Lanka,
-      title: "info.lanka | Services",
+      image: Unseen_Tapro,
+      title: "Unseen_Tapro | in process",
       description:
-        "A centralized, fully responsive web portal designed to streamline access to over 200+ verified Sri Lankan government services through an intuitive keyword-based search engine.",
-      tags: ["HTML5", "CSS", "JavaScript"],
-      link: "https://info-lanka-project.vercel.app",
-      // github: "https://github.com/lakshan-beast/info.lanka-Project",
+        "A comprehensive educational resource hub providing students with organized, downloadable study materials and lesson notes through a minimalist and user-friendly React interface.",
+      tags: ["React.js", "Tailwind CSS", "TypeScript"],
+      link: "https://unseen-tapro.vercel.app/",
+      // github:
+      //   "https://github.com/lakshan-beast/Educa-Advance-Learning-Platform",
       linkedin: "",
     },
 
-    // react zestara project
-    {
-      image: Zestara,
-      title: "Zestara | E-Commerce UI Kit",
-      description:
-        "A premium e-commerce platform built with React and SASS, featuring dynamic product filtering and a seamless checkout experience, optimized for high performance and modern digital shopping.",
-      tags: ["React", "SASS", "Context API"],
-      link: "https://zestara.vercel.app",
-      // github: "https://github.com/lakshan-beast/Zestara",
-      linkedin: "",
-    },
+    // // react zestara project
+    // {
+    //   image: Zestara,
+    //   title: "Zestara | E-Commerce UI Kit",
+    //   description:
+    //     "A premium e-commerce platform built with React and SASS, featuring dynamic product filtering and a seamless checkout experience, optimized for high performance and modern digital shopping.",
+    //   tags: ["React", "SASS", "Context API"],
+    //   link: "https://zestara.vercel.app",
+    //   // github: "https://github.com/lakshan-beast/Zestara",
+    //   linkedin: "",
+    // },
 
     // Dream Track | Academic Productivity Dashboard
     {
@@ -59,16 +61,17 @@ const Projects = () => {
         "https://www.linkedin.com/posts/lakshan-sandeepa_reactjs-firebase-freelanceproject-ugcPost-7459432361013821440-wHDL?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFPtsUwB7_aLX2saPRBkbB618Q_hYVofstU",
     },
 
-    // Frontend Projects
-    // {
-    //   title: "Frontend Projects",
-    //   description:
-    //     "A high-perfomance online shop interface built with React & SASS, focusing on smooth user experience.",
-    //   tags: ["HTML5", "CSS", "JavaScript"],
-    //   github: "https://github.com/lakshan-beast/My-Frontend-Projects-Lab",
-    // },
-
-    // https://future-plan-six.vercel.app/
+    // info.lanka project
+    {
+      image: Info_Lanka,
+      title: "info.lanka | Services",
+      description:
+        "A centralized, fully responsive web portal designed to streamline access to over 200+ verified Sri Lankan government services through an intuitive keyword-based search engine.",
+      tags: ["HTML5", "CSS", "JavaScript"],
+      link: "https://info-lanka-project.vercel.app",
+      // github: "https://github.com/lakshan-beast/info.lanka-Project",
+      linkedin: "",
+    },
   ];
 
   return (
